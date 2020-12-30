864e6b869fdd90f39a5f320ddfa2fad6296d3d5f
luiztools <luizfduartejr@gmail.com>
2020-12-29 23:20:36 -0300

    expire

--- index.js
+++ index.js
@@ -11,6 +11,10 @@
     const result = await client.set("key", "value");
     console.log(`result: ${result}`);
 
+    //exemplos com expiração
+    //const result = await client.set("key", "value", "EX", 60);
+    //await client.expire('key', 60);
+
     const result2 = await client.get("key");
     console.log(`result2: ${result2}`);
 
